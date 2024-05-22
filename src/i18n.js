f1cdf4a3cbcbdf860a84335f1e8c3186a21673c2
Kalijonik <boxajurak@gmail.com>
2024-05-22 11:33:00 +0500

    (fix): localization

--- src/i18n.js
+++ src/i18n.js
@@ -1,32 +1,33 @@
 import { createI18n } from 'vue-i18n';
+import { nextTick } from 'vue';
+import ru from './lang/ru.json';
+import uz from './lang/uz.json';
 
-import { nextTick } from 'vue'
+const i18n = createI18n({
+    legacy: false,
+    locale: 'ru',
+    fallbackLocale: 'uz',
+    messages: {
+        ru,
+        uz,
+    },
+});
 
-
-export default function setupI18n(options = { locale: 'ru' }) {
-  const i18n = createI18n(options)
-  setI18nLanguage(i18n, options.locale)
-  return i18n
+export function setI18nLanguage(i18n, locale) {
+    if (i18n.mode === 'legacy') {
+        i18n.global.locale = locale;
+    } else {
+        i18n.global.locale.value = locale;
+    }
+    document.querySelector('html').setAttribute('lang', locale);
 }
 
-export  function setI18nLanguage(i18n, locale) {
-  if (i18n.mode === 'legacy') {
-    i18n.global.locale = locale
-  } else {
-    i18n.global.locale.value = locale
-  }
-
-  document.querySelector('html').setAttribute('lang', locale)
+export async function loadLocaleMessages(i18n, locale) {
+    const messages = await import(
+        /* webpackChunkName: "locale-[request]" */ `./lang/${locale}.json`
+    );
+    i18n.global.setLocaleMessage(locale, messages.default);
+    return nextTick();
 }
 
-export  async function loadLocaleMessages(i18n, locale) {
-  // load locale messages with dynamic import
-  const messages = await import(
-    /* webpackChunkName: "locale-[request]" */ `./lang/${locale}.json`
-  )
-
-  // set locale and locale message
-  i18n.global.setLocaleMessage(locale, messages.default)
-
-  return nextTick()
-}
+export default i18n;
--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import router from './router/index';
 
 import App from './App.vue';
 import { createBootstrap } from 'bootstrap-vue-next';
-import setupI18n from './i18n';
+import i18n from './i18n';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 import './assets/scss/app.scss';
 import 'bootstrap/dist/js/bootstrap.bundle';
-const i18n = setupI18n({ locale: 'ru' });
 
 
 const app = createApp(App);
